3aa99146812ceeb8a2c3d05a55881f013a881fee
Lucas Matheus <62152683+lucasgomes14@users.noreply.github.com>
2022-10-04 08:01:10 -0300

    Update desafio2.c

--- click do botao/desafio2.c
+++ click do botao/desafio2.c
@@ -1,10 +1,9 @@
 /**
   ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
+ 	 * Projeto: microControlador
+ 	 * Placa: STM32F407VEx
+ 	 * DescriÃ§Ã£o: Ligando e desligando LED
+ 	 * Autor: Lucas Matheus
   ******************************************************************************
 */
 
@@ -20,33 +19,30 @@ int main(void){
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;            //habilita o clock do GPIOA
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;            //habilita o clock do GPIOE
 
-    GPIOA->MODER |= (0b01 << 12);    //configura o pino PA6 como saída
-    //GPIOA->MODER |= (0b01 << 14);    //configura o pino PA6 como saída
+    GPIOA->MODER |= (0b01 << 12);    //configura o pino PA6 como saÃ­da
+    //GPIOA->MODER |= (0b01 << 14);    //configura o pino PA6 como saÃ­da
     GPIOE->MODER &= ~(0b11 << 6);    //configurando o pino PE3 como entrada
-    GPIOE->PUPDR |= (0b01 << 6);    //liga o resistor de pull-up no pino PE3 (pra garanbir nível alto quando a chave estiver solta)
+    GPIOE->PUPDR |= (0b01 << 6);    //liga o resistor de pull-up no pino PE3 (pra garanbir nÃ­vel alto quando a chave estiver solta)
     GPIOE->MODER &= ~(0b11 << 8);    //configurando o pino PE4 como entrada
-    GPIOE->PUPDR |= (0b01 << 8);    //liga o resistor de pull-up no pino PE4 (pra garanbir nível alto quando a chave estiver solta)
+    GPIOE->PUPDR |= (0b01 << 8);    //liga o resistor de pull-up no pino PE4 (pra garanbir nÃ­vel alto quando a chave estiver solta)
 
 
-    //o LED em PA6 só deve acender ao pressionar primeiro K0 e depois K1
+    //o LED em PA6 sÃ³ deve acender ao pressionar primeiro K0 e depois K1
     while(1){
-    	if(!K0 && !K1){
-    	    LED_OFF;
-		}
-    	else{
-    		if(!K0 && K1){
-    			LED_OFF;
-    			while(K1);
-    		}
-    		if(K0 && !K1){
+    	LED_OFF;
+
+	if(!K0 && K1){
+		LED_OFF;
+		while(K1);
+	}
+	if(K0 && !K1){
+		LED_OFF;
+		while(K0){
+			if(K1)
+				LED_ON;
+			else
 				LED_OFF;
-				while(K0){
-					if(K1)
-						LED_ON;
-					else
-						LED_OFF;
-				}
-			}
-    	}
+		}
+	}
     }
 }
